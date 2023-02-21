61f1a56e6923ec73ab4970897d8a5581b24a5224
Quentin <fouilleul.quentin@outlook.fr>
2023-02-21 23:12:53 +0100

    Initial commit

---
+++ src/main/java/fr/quentin/variants/Variants.java
@@ -0,0 +1,17 @@
+package fr.quentin.variants;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Variants implements ModInitializer {
+
+	public static final String MODID = "variants";
+	public static final Logger LOGGER = LoggerFactory.getLogger("Variants");
+	
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/fr/quentin/variants/VariantsClient.java
@@ -0,0 +1,11 @@
+package fr.quentin.variants;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VariantsClient implements ClientModInitializer {
+    
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
